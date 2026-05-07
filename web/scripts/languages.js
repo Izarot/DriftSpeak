ff81b5eae133b530f8b668a7ebc593f4d94d6030
Izarot <sharananandanaraizarot@gmail.com>
2026-05-08 00:20:24 +0530

    Create languages.js

---
+++ web/scripts/languages.js
@@ -0,0 +1,28 @@
+export const fakeLanguages = [
+  "ja",
+  "fr",
+  "de",
+  "tr",
+  "fi",
+  "ar",
+  "ru",
+  "ko",
+  "es"
+];
+
+export const fakeWords = [
+  "semantic",
+  "banana",
+  "quantum",
+  "soup",
+  "cathedral",
+  "microwave",
+  "void",
+  "translation",
+  "orbital",
+  "fish",
+  "memory",
+  "signal",
+  "drift",
+  "echo"
+];
